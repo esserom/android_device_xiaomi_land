59fcae261a76d9a158056bf03683d3ff6821912d
sweetyicecare <gabrielseedev@outlook.com>
2020-02-27 06:11:52 +0000

    land: ignore neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,6 +225,7 @@ TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
