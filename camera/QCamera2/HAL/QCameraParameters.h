cf89400261f02dc5c8a2a23afd72efa2996ac407
sweetyicecare <gabrielseedev@outlook.com>
2020-02-27 03:31:20 +0000

    Revert "land: Replace libcamera_client to android.hardware.camera.common@1.0-helper"
    This reverts commit 495744cacb1f1e5d19d606640e6b624b8237806b.

--- camera/QCamera2/HAL/QCameraParameters.h
+++ camera/QCamera2/HAL/QCameraParameters.h
@@ -24,7 +24,7 @@
 #include <cutils/properties.h>
 
 // System dependencies
-#include <CameraParameters.h>
+//#include <camera/CameraParameters.h>
 #include <utils/Errors.h>
 
 // Camera dependencies
@@ -32,13 +32,12 @@
 #include "QCameraMem.h"
 #include "QCameraParametersIntf.h"
 #include "QCameraThermalAdapter.h"
+#include "CameraParameters.h"
 
 extern "C" {
 #include "mm_jpeg_interface.h"
 }
 
-using ::android::hardware::camera::common::V1_0::helper::CameraParameters;
-using ::android::hardware::camera::common::V1_0::helper::Size;
 using namespace android;
 
 namespace qcamera {
