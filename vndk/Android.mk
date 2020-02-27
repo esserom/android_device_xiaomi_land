2ddf621265dab09936cd3c71ae9554b1f4b58a20
sweetyicecare <gabrielseedev@outlook.com>
2020-02-27 03:28:03 +0000

    Revert "land: Migrate to versioned VNDK layout"
    This reverts commit d51bd0d55e72573c7163f43b3ba26d4868a0d992.

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -4,8 +4,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/vndk-sp-libs.mk
 
-vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
-
 define define-vndk-sp-lib
 include $$(CLEAR_VARS)
 LOCAL_MODULE := $1.vndk-sp-gen
@@ -17,7 +15,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)
+LOCAL_MODULE_RELATIVE_PATH := vndk-sp
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -32,7 +30,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)
+LOCAL_MODULE_RELATIVE_PATH := vndk-sp
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
@@ -47,5 +45,4 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
 
-vndk_sp_dir :=
 endif
